6c9c51ff7cde39a460759fbd576f42ad169dd395
dawidmarek95x <dawid.marek95x@gmail.com>
2022-12-07 22:06:50 +0100

    Basic routing along with spinners and support for unknown routes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import NotFound from './pages/NotFound/NotFound';
 import './index.scss';
+
+const Dashboard = React.lazy(() => import("./pages/Dashboard/Dashboard"));
+const Landing = React.lazy(() => import("./pages/Landing/Landing"));
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -11,7 +16,15 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Dashboard />} />
+            <Route path="landing" element={<Landing />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
