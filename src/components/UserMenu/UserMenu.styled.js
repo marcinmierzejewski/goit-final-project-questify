395b8a1b36ded19c63798cb10df63306cab885af
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2022-12-08 23:15:34 +0100

    used props

--- src/components/UserMenu/UserMenu.styled.js
+++ src/components/UserMenu/UserMenu.styled.js
@@ -7,7 +7,7 @@ export const UserMenuWrapper = styled.div`
 `;
 
 export const TrophyActive = styled.p`
-  background: #00d7ff;
+  background-color: ${props => props.isActive === true ? "#00d7ff" : "#3E4E6C"};
   height: 30px;
   width: 30px;
   border-radius: 50%;
@@ -16,7 +16,7 @@ export const TrophyActive = styled.p`
   justify-content: center;
   margin-right: 38px;
   position: relative;
-  fill: "#242A37";
+  fill: ${props => props.isActive  === true ? "#242A37" : "#7B8AA4"};
 
   @media screen and (max-width: 500px) {
     margin-right: 10px;
@@ -36,37 +36,6 @@ export const TrophyActive = styled.p`
     z-index: -1;
   }
 `;
-
-export const TrophyNotActive = styled.p`
-  background: #3E4E6C;
-  height: 30px;
-  width: 30px;
-  border-radius: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-right: 38px;
-  position: relative;
-  fill: #7B8AA4;
-
-  @media screen and (max-width: 500px) {
-    margin-right: 10px;
-  }
-
-  &::after {
-    content: "";
-    display: block;
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 30px;
-    height: 30px;
-    border-radius: 50%;
-    background: #00d7ff;
-    filter: blur(4px);
-    z-index: -1;
-  }
-`
 
 export const LogOutBtn = styled.button`
   background: transparent;
