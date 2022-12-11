6922d5a85ed8dd25832a94f8cd3e0cf9030deb3c
BartlomiejKita <bartlomiejpawelkita@gmail.com>
2022-12-11 21:10:08 +0100

    fix background to white

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -2,8 +2,12 @@ import LandingWelcome from "./LandingWelcome/LandingWelcome";
 import LandingForm from "./LandingForm/LandingForm";
 import LandingBg from "./LandingBg/LandingBg";
 import { Container, Wrapper } from "./Landing.styled";
+import { useEffect } from "react";
 
 const Landing = () => {
+	useEffect(() => {
+		document.body.style.backgroundColor = "white";
+	}, []);
 	return (
 		<Wrapper>
 			<Container>
