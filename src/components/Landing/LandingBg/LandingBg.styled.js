02222fbe00ff25da3f72e5855e1718d5062c3966
BartlomiejKita <bartlomiejpawelkita@gmail.com>
2022-12-17 15:37:22 +0100

    refactor

--- src/components/Landing/LandingBg/LandingBg.styled.js
+++ src/components/Landing/LandingBg/LandingBg.styled.js
@@ -5,15 +5,7 @@ export const ImgUp = styled.img`
 	bottom: 0;
 	z-index: -1;
 
-	@media screen and (min-width: 768px) and (max-width: 1020px) {
-		top: 0;
-		right: 0;
-	}
-	@media screen and (min-width: 1020px) and (max-width: 1280px) {
-		top: 0;
-		right: 0;
-	}
-	@media screen and (min-width: 1280px) {
+	@media screen and (min-width: 768px) {
 		top: 0;
 		right: 0;
 	}
@@ -23,15 +15,7 @@ export const ImgDown = styled.img`
 	position: absolute;
 	bottom: 0;
 	z-index: -1;
-	@media screen and (min-width: 768px) and (max-width: 1019px) {
-		bottom: 0;
-		right: 0;
-	}
-	@media screen and (min-width: 1020px) and (max-width: 1279px) {
-		bottom: 0;
-		right: 0;
-	}
-	@media screen and (min-width: 1280px) {
+	@media screen and (min-width: 768px) {
 		bottom: 0;
 		right: 0;
 	}
