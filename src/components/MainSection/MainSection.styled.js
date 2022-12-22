050008baea7c1244b189a2d348cdb6cae72f6a96
Anna <anna.ojdana@gmail.com>
2022-12-22 17:10:14 +0100

    padding w main

--- src/components/MainSection/MainSection.styled.js
+++ src/components/MainSection/MainSection.styled.js
@@ -5,10 +5,10 @@ export const Main = styled.main`
   overflow: hidden;
 
   @media screen and (min-width: 768px) {
-    padding: 0 49px 0 50px;
+    padding: 30px 49px 0 50px;
   }
 
   @media screen and (min-width: 1280px) {
-    padding: 0 72px 0 71px;
+    padding: 30px 72px 0 71px;
   }
 `;
