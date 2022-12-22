897142ef46c1e7af89f692c7e35bafee2975b25e
BartlomiejKita <bartlomiejpawelkita@gmail.com>
2022-12-22 12:51:09 +0100

    add cookie modal

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -3,20 +3,24 @@ import LandingForm from "./LandingForm/LandingForm";
 import LandingBg from "./LandingBg/LandingBg";
 import { Container, Wrapper } from "./Landing.styled";
 import { useEffect } from "react";
+import Cookie from "../Cookie/Cookie";
 
 const Landing = () => {
-  useEffect(() => {
-    document.body.style.backgroundColor = "white";
-  }, []);
-  return (
-    <Wrapper>
-      <Container>
-        <LandingWelcome />
-        <LandingForm />
-      </Container>
-      <LandingBg />
-    </Wrapper>
-  );
+	useEffect(() => {
+		document.body.style.backgroundColor = "white";
+	}, []);
+	return (
+		<>
+			<Wrapper>
+				<Container>
+					<LandingWelcome />
+					<LandingForm />
+				</Container>
+				<LandingBg />
+			</Wrapper>
+			<Cookie />
+		</>
+	);
 };
 
 export default Landing;
